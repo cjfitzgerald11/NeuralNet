de552585a64cb7ce54148a5d786f8a7d0cc5964e
Connor FitzGerald <cjfitzgerald11@gmail.com>
2019-11-29 15:47:08 -0600

    32x32

--- ImageReader.py
+++ ImageReader.py
@@ -8,6 +8,18 @@ class ImageReader:
         if len(self.images) > 0:
             continue
         else:
+            file = open(self.imageFolder, 'r+')
+            input = file.readLines()
+            input = [line.strip("\n") for line in input]
+            for i in range(3, len(input), 32):
+                fileString = ''
+                for v in range(32):
+                    fileString += input[i + v]
+                self.images += fileString
+                self.answers += input[i + 32]
+
+
+
             #reads each image in as 1d list of 0 and 1 ints
             #for each image adds is answer to the answers matrix
             #this method does not need to return anything
